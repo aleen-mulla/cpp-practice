4fe3c1b04700eca3024d737ceed29bab0d4b99f7
Aleen Mulla <aleenmulla08@gmail.com>
2026-08-10 16:49:55 +0530

    Add sq. cube program

---
+++ SQ_CUBE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+clrscr();
+int a,b,c;
+printf("enter input");
+scanf("%d",&a);
+b=a*a;
+c=a*a*a;
+printf("\nsquare=%d",b);
+printf("\nCube=%d",c);
+getch();
+}
