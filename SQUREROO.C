444da689c6bad0ecb328bd2fcae0479a5fc26208
Aleen Mulla <aleenmulla08@gmail.com>
2026-08-10 16:49:09 +0530

    Add squreroot program

---
+++ SQUREROO.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+clrscr();
+float a,b;
+printf("Enter value=");
+scanf("%f",&a);
+b=sqrt(a);
+printf("Squre root=%f",b);
+getch();
+}
