3041f55cac59b0f904e8ff1fc22c07c8a9aa5642
Aleen Mulla <aleenmulla08@gmail.com>
2026-08-10 16:47:38 +0530

    Add lower char program

---
+++ LOWER.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+clrscr();
+char a;
+printf("Enter Character in uppeer case=");
+scanf("%c",&a);
+printf("\ncharacter in lower case=%c",a+32);
+getch();
+}
