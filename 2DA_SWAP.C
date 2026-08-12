7cdb659f526cc867244f8fa5b70de46d2f4d813f
Aleen Mulla <aleenmulla08@gmail.com>
2026-08-12 12:56:04 +0530

    Add 2d array swap program

---
+++ 2DA_SWAP.C
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+clrscr();
+int a[2][2],b[2][2],c[4][2],i,j;
+printf("1st array=");
+for(i=0;i<=1;i++)
+{
+for(j=0;j<=1;j++)
+{
+scanf("\n%d",&a[i][j]);
+c[i+2][j]=a[i][j];
+}
+}
+printf("\n2 nd array=");
+for(i=0;i<=1;i++)
+{
+for(j=0;j<=1;j++)
+{
+scanf("\n%d",&b[i][j]);
+c[i][j]=b[i][j];
+}
+}
+printf("\n");
+printf("\nOutput=\n");
+for(i=0;i<=3;i++)
+{
+for(j=0;j<=1;j++)
+{
+printf("\t%d",c[i][j]);
+}
+printf("\n");
+}
+getch();
+}
