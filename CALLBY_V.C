9e09b256087d050afdedc7e912de813efc61a02f
Aleen Mulla <aleenmulla08@gmail.com>
2026-08-13 19:13:33 +0530

    Add call by value program

---
+++ CALLBY_V.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void swap(int,int);
+
+main()
+{
+clrscr();
+int a,b;
+printf("Enter no.=\n");
+scanf("%d%d",&a,&b);
+swap(a,b);
+getch();
+}
+void swap(int c,int d)
+{
+int temp;
+temp=c;
+c=d;
+d=temp;
+printf("Nos after swapping=\n%d\n%d",c,d);
+}
